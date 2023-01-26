469013a188f57ee515cf0b2f46d9055332d665d9
AliaHa3 <eng.alia.hamwi@gmail.com>
2023-01-26 20:46:19 +0200

    commands

---
+++ week_1_basics_n_setup/1_terraform_gcp/Hw1-b commands.sh
@@ -0,0 +1,25 @@
+ssh -i ~/.ssh/gcp alia@34.22.131.236
+
+
+wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
+
+
+
+# ###########
+ssh -i ~/.ssh/gcp alia@34.22.131.236
+
+touch config
+
+############
+
+ssh de-zoomcamp
+
+#################
+
+export GOOGLE_APPLICATION_CREDENTIALS="D:\Study\data-engineering-zoomcamp\week_1_basics_n_setup\dezoomcamp-375819-433f99121d3e.json"
+gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
+
+##################
+
+export GOOGLE_APPLICATION_CREDENTIALS=~/.gc/dezoomcamp-375819-433f99121d3e.json
+gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
